e4d5a2f9e32f00d70bdf37ad861921f74ceba62b
shubhamc <shubh.chodankar@gmail.com>
2016-09-14 19:32:00 +0530

    Adds section on landing page for three sections: about us, our work, contact us

--- client/src/js/homeController.js
+++ client/src/js/homeController.js
@@ -1,4 +1,4 @@
 //main controller for the landing page of the application
 app.controller('HomeCtrl', function($scope){
-	$scope.test = 'Welcome shubham, angular basic app has been setup!';
+	$scope.test = 'We deliver quality and smile to our customers!';
 });
