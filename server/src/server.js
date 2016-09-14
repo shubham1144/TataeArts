192b558ffdf35b1e569d65d4bf852b6c01ab3186
shubhamc <shubh.chodankar@gmail.com>
2016-09-14 12:15:23 +0530

    Initial setup for server to handle requests

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+//Create a server using express
+var express = require('express');
+var app = express();
+var path = require('path');
+var PORT = process.env.PORT || 3000;
+//We need to set location of our static files
+app.use(express.static(path.join(__dirname, "../../client")));
+
+//Configure our basic server to respond index page
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, '../../client', 'index.html')); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+//Start the server
+app.listen(PORT);
+console.log('Server is up and running on port ' + PORT);
