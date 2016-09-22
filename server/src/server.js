6e10c9e11e45aef505e6c9f5e86d22801c3b37b6
shubhamc <shubh.chodankar@gmail.com>
2016-09-22 15:22:46 +0530

    Adds API for AskQueries
    * Inserts the query data in mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -10,14 +10,36 @@ var moment = require('moment');
 moment().format();
 var multer = require('multer');
 var upload = multer();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+//Sets middleware to json body
+app.use(bodyParser.json());
 //Setting the port as 80 as we have configured 80 as default port for http on EC2 instance
 var PORT = process.env.PORT || 80;
 //We need to set location of our static files
 app.use(express.static(path.join(__dirname, "../../client")));
 //Adding a favicon for the website 
 app.use(favicon(path.join(__dirname,'../../client','src/img','tataelogo.jpg')));
+//Setup connection with mongodb instance
+mongoose.connect('mongodb://localhost/tataearts');
+//Logic to validate successfull connection with mongodb
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to mongodb successfully...");
+});
+//Create a Schema for CustomerQuery
+var customerQuery = mongoose.Schema({
+    name :           String,
+    contactNumber :  Number,
+    description :    String
+});
+//Construct a model from schema
+var CustomerQuery = mongoose.model('CustomerQuery', customerQuery);
+
 //Set Aws Configuration
 aws.config.loadFromPath(path.join(__dirname, './AwsConfig.json'));
+
 //Logic to Upload files to AWS S3 Bucket
 var s3 = new aws.S3();
 //API for accepting files uploaded from client
@@ -33,8 +55,19 @@ app.post('/addArtImages', upload.any(), function(req, res){
 
 //API to get list of Art Images on s3
 app.get('/getArtImages', function(req, res) {
-  console.log("Trying to fetch list of images...");
+  //console.log("Trying to fetch list of images...");
   getListOfObjects(res);
+});
+
+//API to raise customer queries and store in mongodb
+app.post('/askQuery', function(req, res){
+  console.log("Trying to store the query raised in database...");
+  console.log("The content being received as query is : " + req.body);
+  var query = new CustomerQuery(req.body);
+  query.save(function (err) {
+    if (err) return handleError(err);
+    res.sendStatus(200);
+  });
 });
 
 //API to return admin panel
